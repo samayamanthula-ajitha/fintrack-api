3331c1d027bef336756ee120029cf51d9a775d53
samayamanthula-ajitha <samayamanthulaajitha@gmail.com>
2026-08-16 18:35:13 +0530

    test: add Jest configuration

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests', '<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts',
+    '!src/database.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: {
+        strict: true,
+        esModuleInterop: true,
+      },
+    },
+  },
+  verbose: true,
+  testTimeout: 10000,
+};
